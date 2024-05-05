7dbd6262eecd243507b26c354ea77e310036bcfc
daniellungu <lungud63@yahoo.com>
2024-05-05 21:32:22 +0300

    changed architecture for admin ft

--- src/main/java/org/danielsa/proiect_ps/controller/AdminController.java
+++ src/main/java/org/danielsa/proiect_ps/controller/AdminController.java
@@ -1,68 +1,77 @@
 package org.danielsa.proiect_ps.controller;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.scene.control.TableView;
 import lombok.Getter;
-import lombok.Setter;
 import org.danielsa.proiect_ps.model.AdminModel;
 import org.danielsa.proiect_ps.model.UserModel;
+import org.danielsa.proiect_ps.view.AdminView;
 
 import java.util.ArrayList;
 
 @Getter
 public class AdminController {
     private final AdminModel model;
-    private final StringProperty usernameProperty = new SimpleStringProperty();
-    private final StringProperty passwordProperty = new SimpleStringProperty();
-    private final ObjectProperty<String> userTypeProperty = new SimpleObjectProperty<>();
-    @Setter
-    private ObjectProperty<UserModel> selectedUserProperty = new SimpleObjectProperty<>();
-    @Setter
-    private ObjectProperty<TableView<UserModel>> userTableViewProperty = new SimpleObjectProperty<>();
+    private final AdminView view;
 
     public AdminController() {
         this.model = new AdminModel();
+        this.view = new AdminView();
+        initComponents();
+    }
+
+    private void initComponents() {
+        view.getAddButton().setOnAction(event -> addUser());
+        view.getUpdateButton().setOnAction(event -> {
+            if (view.getUserTableView().getSelectionModel().getSelectedItem() != null) {
+                updateUser();
+            }
+        });
+        view.getDeleteButton().setOnAction(event -> {
+            if (view.getUserTableView().getSelectionModel().getSelectedItem() != null) {
+                deleteUser();
+            }
+        });
+        view.getUserTableView().getItems().addAll(getUsers());
+        view.getUserTableView().getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> view.setSelectedUser(newVal));
+
     }
 
     public void addUser() {
         boolean success = model.register(
-                getUsernameProperty().getValue(),
-                getPasswordProperty().getValue(),
-                getUserTypeProperty().getValue()
+                view.getUserNameField().getText(),
+                view.getPasswordField().getText(),
+                view.getUserTypeComboBox().getValue()
         );
 
         if (success) {
-            TableView<UserModel> tableView = getUserTableViewProperty().getValue();
-            tableView.getItems().addAll(model.getUserByUsername(getUsernameProperty().getValue()));
+            TableView<UserModel> tableView = view.getUserTableView();
+            tableView.getItems().addAll(model.getUserByUsername(view.getUserNameField().getText()));
         } else {
             System.out.println("User not added!");
         }
     }
 
     public void updateUser() {
-        UserModel updatedUser = model.updateUser(getSelectedUserProperty().getValue().getUserName(), getUsernameProperty().getValue(), getPasswordProperty().getValue(), getUserTypeProperty().getValue());
+        UserModel updatedUser = model.updateUser(view.getSelectedUser().getUserName(), view.getUserNameField().getText(), view.getPasswordField().getText(), view.getUserTypeComboBox().getValue());
 
         if (updatedUser == null) {
             System.out.println("User not updated!");
             return;
         }
 
-        getUserTableViewProperty().getValue().getItems().clear();
-        getUserTableViewProperty().getValue().getItems().addAll(model.getUsers());
+        view.getUserTableView().getItems().clear();
+        view.getUserTableView().getItems().addAll(model.getUsers());
     }
 
     public void deleteUser() {
-        boolean success = model.deleteUser(getSelectedUserProperty().getValue().getUserName());
+        boolean success = model.deleteUser(view.getSelectedUser().getUserName());
 
         if (!success) {
             System.out.println("User not deleted!");
             return;
         }
 
-        getUserTableViewProperty().getValue().getItems().remove(getUserTableViewProperty().getValue().getSelectionModel().getSelectedItem());
+        view.getUserTableView().getItems().remove(view.getUserTableView().getSelectionModel().getSelectedItem());
     }
 
     public ArrayList<UserModel> getUsers(){
--- src/main/java/org/danielsa/proiect_ps/view/AdminView.java
+++ src/main/java/org/danielsa/proiect_ps/view/AdminView.java
@@ -1,36 +1,39 @@
 package org.danielsa.proiect_ps.view;
 
-import javafx.beans.binding.Bindings;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import lombok.Getter;
+import lombok.Setter;
 import org.danielsa.proiect_ps.model.UserModel;
-import org.danielsa.proiect_ps.controller.AdminController;
 import org.danielsa.proiect_ps.utils.LanguageManager;
 
 import java.util.Arrays;
 
+@Getter
 public class AdminView extends Scene implements Observer {
-    private final AdminController controller;
+    private final TextField userNameField = new TextField();
+    private final PasswordField passwordField = new PasswordField();
+    private final ComboBox<String> userTypeComboBox = new ComboBox<>();
+    private final Button addButton = new Button(LanguageManager.getString("addButton"));
+    private final Button updateButton = new Button(LanguageManager.getString("updateButton"));
+    private final Button deleteButton = new Button(LanguageManager.getString("deleteButton"));
+    private final TableView<UserModel> userTableView = new TableView<>();
+    @Setter
+    private UserModel selectedUser;
+
 
     public AdminView() {
         super(new VBox(), 500, 500);
-        this.controller = new AdminController();
         initComponents();
     }
 
     public void initComponents() {
-        TextField userNameField = new TextField();
-        PasswordField passwordField = new PasswordField();
-        ComboBox<String> userTypeComboBox = new ComboBox<>();
+
         userTypeComboBox.getItems().addAll(LanguageManager.getStringForKey("userTypeComboBox", "valueAdmin"), LanguageManager.getStringForKey("userTypeComboBox", "valuePlayer"));
-        Button addButton = new Button(LanguageManager.getString("addButton"));
-        Button updateButton = new Button(LanguageManager.getString("updateButton"));
-        Button deleteButton = new Button(LanguageManager.getString("deleteButton"));
-        TableView<UserModel> userTableView = new TableView<>();
 
         TableColumn<UserModel, String> userNameColumn = new TableColumn<>(LanguageManager.getString("userNameColumn"));
         userNameColumn.setCellValueFactory(new PropertyValueFactory<>("userName"));
@@ -39,26 +42,6 @@ public class AdminView extends Scene implements Observer {
         TableColumn<UserModel, Integer> gamesWonColumn = new TableColumn<>(LanguageManager.getString("gamesWonColumn"));
         gamesWonColumn.setCellValueFactory(new PropertyValueFactory<>("gamesWon"));
         userTableView.getColumns().addAll(Arrays.asList(userNameColumn, userTypeColumn, gamesWonColumn));
-
-        userTableView.getItems().addAll(controller.getUsers());
-
-        controller.getUserTableViewProperty().set(userTableView);
-
-        userTableView.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> controller.getSelectedUserProperty().set(newVal));
-
-        addButton.setOnAction(event -> controller.addUser());
-
-        updateButton.setOnAction(event -> {
-            if (userTableView.getSelectionModel().getSelectedItem() != null) {
-                controller.updateUser();
-            }
-        });
-
-        deleteButton.setOnAction(event -> {
-            if (userTableView.getSelectionModel().getSelectedItem() != null) {
-                controller.deleteUser();
-            }
-        });
 
         VBox root = new VBox();
         root.setSpacing(10);
@@ -70,10 +53,6 @@ public class AdminView extends Scene implements Observer {
                 new HBox(addButton, updateButton, deleteButton),
                 userTableView
         );
-
-        Bindings.bindBidirectional(userNameField.textProperty(), controller.getUsernameProperty());
-        Bindings.bindBidirectional(passwordField.textProperty(), controller.getPasswordProperty());
-        Bindings.bindBidirectional(userTypeComboBox.valueProperty(), controller.getUserTypeProperty());
 
         setRoot(root);
     }
